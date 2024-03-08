cb70e743e91f9ded4cec4473634167dd5b6a4aab
F Beaumont <fbeaumont@bigetnouf.fr>
2024-03-08 13:02:40 +0100

    feat(chore): ajout de l'installeur Windows

---
+++ Better_FR_Windows.cmd
@@ -0,0 +1,158 @@
+:: Balatro French Translations
+::
+:: Script d'installation du pack de langue FR pour Balatro
+:: Fichier de langue et assets créés par la communauté Discord (Balatro FR - loc mod) : https://discord.gg/kQMdHTXB3Z
+:: Toutes les sources à jour sont disponibles ici : https://github.com/FrBmt-BIGetNouf/balatro-french-translations/
+::
+:: Ce script utilise Balamod pour injecter les ressources au jeu (https://github.com/UwUDev/balamod)
+
+@echo off
+setlocal enabledelayedexpansion
+
+set "colorReset=[0m"
+set "resourcesFolder=Balatro_Localization_Resources"
+
+echo =========================================
+echo ==     Balatro French Translations     ==
+echo ==  Installation du pack de langue FR  ==
+echo ==        Traductions et images        ==
+echo =========================================
+
+:: Question utilisateur : Les images en Francais doivent-elles être utilisées ?
+echo.
+echo.
+choice /C ON /M "Voulez-vous utiliser les images en Francais ?"
+if errorlevel 2 (
+    echo Les images ne seront pas ajoutees
+    set "download_assets=false"
+)   else (
+    echo Les images seront ajoutees
+    set "download_assets=true"
+)
+
+:: Vérificitation de l'installation par défaut de Steam (via libraryfolders.vdf  sur C:)
+set "steamLibraryFile=C:\Program Files (x86)\Steam\steamapps\libraryfolders.vdf"
+
+:: S'il n'existe pas, ouverture de l'explorer pour la sélection manuelle de Balatro.exe
+if not exist "!steamLibraryFile!" (
+    echo.
+    echo Oups, merci de nous indiquer ou se trouve Balatro.exe
+
+    set "balatroFile="
+    set "dialogTitle=Selectionner balatro.exe"
+    set "fileFilter=Balatro Executable (balatro.exe) | balatro.exe"
+
+    for /f "delims=" %%I in ('powershell -Command "& { Add-Type -AssemblyName System.Windows.Forms; $dlg = New-Object System.Windows.Forms.OpenFileDialog; $dlg.Filter = '!fileFilter!'; $dlg.Title = '!dialogTitle!'; $dlg.ShowHelp = $true; $dlg.ShowDialog() | Out-Null; $dlg.FileName }"') do set "selectedFile=%%I"
+    
+    if defined selectedFile (
+        set "balatroFile=!selectedFile!"
+        echo Balatro.exe : !balatroFile!
+    ) else (
+        echo Balatro.exe : Fichier non selectionne. Installation annulee
+        goto :fin
+    )
+)
+
+:: Création des dossiers de ressources temporaire
+if not exist "%resourcesFolder%" mkdir "%resourcesFolder%"
+if not exist "%resourcesFolder%\assets" mkdir "%resourcesFolder%\assets"
+if not exist "%resourcesFolder%\assets\1x" mkdir "%resourcesFolder%\assets\1x"
+if not exist "%resourcesFolder%\assets\2x" mkdir "%resourcesFolder%\assets\2x"
+
+:: Récupération de nom de la dernière release de Balamod
+for /f %%a in ('powershell -command "$tag = (Invoke-RestMethod -Uri 'https://api.github.com/repos/UwUDev/balamod/releases/latest').tag_name; $tag"') do set latestTag=%%a
+
+:: Creation des noms et liens des fichiers. Valable uniquement tant que le fichier windows s'appelle bien balamod-v.y.z-windows.exe.
+set "balamodFile=balamod-%latestTag%-windows.exe"
+set "balamodFileUrl=https://github.com/UwUDev/balamod/releases/download/%latestTag%/%balamodFile%"
+set "fr_repository=https://raw.githubusercontent.com/FrBmt-BIGetNouf/balatro-french-translations/main/localization"
+set "fr_translation=%fr_repository%/fr.lua"
+set "fr_assetsBoosters1x=%fr_repository%/assets/1x/boosters.png"
+set "fr_assetsBoosters2x=%fr_repository%/assets/2x/boosters.png"
+set "fr_assetsTarots1x=%fr_repository%/assets/1x/Tarots.png"
+set "fr_assetsTarots2x=%fr_repository%/assets/2x/Tarots.png"
+set "fr_assetsVouchers1x=%fr_repository%/assets/1x/Vouchers.png"
+set "fr_assetsVouchers2x=%fr_repository%/assets/2x/Vouchers.png"
+set "fr_assetsIcons1x=%fr_repository%/assets/1x/icons.png"
+set "fr_assetsIcons2x=%fr_repository%/assets/2x/icons.png"
+
+:: Téléchargement de Balamod
+if not exist "%resourcesFolder%\%balamodFile%" (
+    echo.
+    echo Telechargement de Balamod...
+    echo.
+    curl -L -o "%resourcesFolder%\%balamodFile%" %balamodFileUrl%
+    echo.
+    echo Telechargement de Balamod termine
+    echo.
+)
+
+:: Téléchargement du pack de langue FR
+echo.
+echo Telechargement du mod FR...
+echo.
+curl -L -o "%resourcesFolder%\fr.lua" %fr_translation%
+
+if "%download_assets%"=="true" (
+    curl -L -o "%resourcesFolder%\assets\1x\boosters.png" %fr_assetsBoosters1x%
+    curl -L -o "%resourcesFolder%\assets\2x\boosters.png" %fr_assetsBoosters2x%
+    curl -L -o "%resourcesFolder%\assets\1x\Tarots.png" %fr_assetsTarots1x%
+    curl -L -o "%resourcesFolder%\assets\2x\Tarots.png" %fr_assetsTarots2x%
+    curl -L -o "%resourcesFolder%\assets\1x\Vouchers.png" %fr_assetsVouchers1x%
+    curl -L -o "%resourcesFolder%\assets\2x\Vouchers.png" %fr_assetsVouchers2x%
+    curl -L -o "%resourcesFolder%\assets\1x\icons.png" %fr_assetsIcons1x%
+    curl -L -o "%resourcesFolder%\assets\2x\icons.png" %fr_assetsIcons2x%
+)
+
+echo.
+echo Telechargement du mod FR termine
+echo.
+
+:: Injection du pack de langue FR
+echo.
+echo Installation du pack de langue...
+echo.
+
+
+if not defined balatroFile (
+    :: Si Steam installé par défaut, on laisse Balamod chercher le fichier Balatro.
+    "./%resourcesFolder%\%balamodFile%" -x -i .\%resourcesFolder%\fr.lua -o localization/fr.lua
+    if "%download_assets%"=="true" (
+        "./%resourcesFolder%\%balamodFile%" -x -i .\%resourcesFolder%\assets\1x\boosters.png -o resources/textures/1x/boosters.png
+        "./%resourcesFolder%\%balamodFile%" -x -i .\%resourcesFolder%\assets\2x\boosters.png -o resources/textures/2x/boosters.png
+        "./%resourcesFolder%\%balamodFile%" -x -i .\%resourcesFolder%\assets\1x\Tarots.png -o resources/textures/1x/Tarots.png
+        "./%resourcesFolder%\%balamodFile%" -x -i .\%resourcesFolder%\assets\2x\Tarots.png -o resources/textures/2x/Tarots.png
+        "./%resourcesFolder%\%balamodFile%" -x -i .\%resourcesFolder%\assets\1x\Vouchers.png -o resources/textures/1x/Vouchers.png
+        "./%resourcesFolder%\%balamodFile%" -x -i .\%resourcesFolder%\assets\2x\Vouchers.png -o resources/textures/2x/Vouchers.png
+        "./%resourcesFolder%\%balamodFile%" -x -i .\%resourcesFolder%\assets\1x\icons.png -o resources/textures/1x/icons.png
+        "./%resourcesFolder%\%balamodFile%" -x -i .\%resourcesFolder%\assets\2x\icons.png -o resources/textures/2x/icons.png
+    )
+) else (
+    :: Sinon on lui envoie le dossier du fichier Balatro.exe selectionné précédemment.
+    for %%A in ("!balatroFile!") do set "balatroFolder=%%~dpA"
+    "./%resourcesFolder%\%balamodFile%" -b !balatroFolder! -x -i .\%resourcesFolder%\fr.lua -o localization/fr.lua
+    if "%download_assets%"=="true" (
+        "./%resourcesFolder%\%balamodFile%" -b !balatroFolder! -x -i .\%resourcesFolder%\assets\1x\boosters.png -o resources/textures/1x/boosters.png
+        "./%resourcesFolder%\%balamodFile%" -b !balatroFolder! -x -i .\%resourcesFolder%\assets\2x\boosters.png -o resources/textures/2x/boosters.png
+        "./%resourcesFolder%\%balamodFile%" -b !balatroFolder! -x -i .\%resourcesFolder%\assets\1x\Tarots.png -o resources/textures/1x/Tarots.png
+        "./%resourcesFolder%\%balamodFile%" -b !balatroFolder! -x -i .\%resourcesFolder%\assets\2x\Tarots.png -o resources/textures/2x/Tarots.png
+        "./%resourcesFolder%\%balamodFile%" -b !balatroFolder! -x -i .\%resourcesFolder%\assets\1x\Vouchers.png -o resources/textures/1x/Vouchers.png
+        "./%resourcesFolder%\%balamodFile%" -b !balatroFolder! -x -i .\%resourcesFolder%\assets\2x\Vouchers.png -o resources/textures/2x/Vouchers.png
+        "./%resourcesFolder%\%balamodFile%" -b !balatroFolder! -x -i .\%resourcesFolder%\assets\1x\icons.png -o resources/textures/1x/icons.png
+        "./%resourcesFolder%\%balamodFile%" -b !balatroFolder! -x -i .\%resourcesFolder%\assets\2x\icons.png -o resources/textures/2x/icons.png
+    )
+)
+
+echo %colorReset%
+echo.
+echo Installation du pack de langue terminee
+
+:: Suppression des fichiers ressources
+rd /s /q "%resourcesFolder%"
+echo Balatro a ete mis a jour !
+
+:fin
+echo.
+echo.
+echo.
+pause
